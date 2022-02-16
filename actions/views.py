90820cdc73ec95737d49e8becdbc800a5e4f1b6c
Vitalii Halkin <vitalii@squash.io>
2022-02-16 19:48:38 +0200

    Add index view

--- actions/views.py
+++ actions/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ActionSerializer
 from .models import Action
+from django.http import HttpResponse
 
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 class ActionViewSet(viewsets.ModelViewSet):
     queryset = Action.objects.all()
--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from actions import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('actions.urls')),
+    path('', views.index, name='index'),
 ]
